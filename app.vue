849d3a2f1b3d74b6026fd9d47d8ef5a6b183b6f6
Maulana <maulanayusupp@gmail.com>
2024-09-28 12:48:13 +0700

    Setup project

---
+++ app.vue
@@ -0,0 +1,32 @@
+<template>
+	<!-- Layout -->
+	<NuxtLayout name="custom">
+		<!-- Page -->
+		<NuxtPage/>
+	</NuxtLayout>
+</template>
+
+<script setup>
+    const config = useRuntimeConfig()
+</script>
+
+<script>
+
+export default {
+	components: {
+	},
+	props: {
+	},
+	data() {
+		return {
+		}
+	},
+	created() {},
+	mounted() {
+    },
+	beforeUnmount() {},
+	methods: {},
+	computed: {},
+	watch: {},
+}
+</script>
